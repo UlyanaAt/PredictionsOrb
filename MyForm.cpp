167f35c3cd8714451363f0db8636b6a681d34c5e
Ульяна Атанова <ula122002@gmail.com>
2023-04-20 13:32:57 +0700

    adjustments

--- MyForm.cpp
+++ MyForm.cpp
@@ -18,49 +18,60 @@ void main(array<String^>^ args) {
 
 System::Void PredictionsOrb::MyForm::button1_Click(System::Object^ sender, System::EventArgs^ e)
 {
-    int RandNumb = 0;
-    RandNumb = 1 + rand() % 100;
+    int k = 0;
+    double RandNumb = 0;
+    RandNumb = (1 + rand() % 100);
+    RandNumb = RandNumb / 100;
 
     label1->Text = "";
 
-    if (RandNumb <= 12.5)
+    while (RandNumb > 0)
     {
-        label1->Text = "Yes, definitely";
-        label1->ForeColor = Color::Green;
+        RandNumb = RandNumb - 0.125;
+        k += 1;
     }
-    else if (RandNumb > 12.5 && RandNumb <= 25)
+
+    switch (k)
     {
-        label1->Text = "As I see it, yes";
-        label1->ForeColor = Color::Green;
+        case 1:
+            label1->Text = "Yes, definitely";
+            label1->ForeColor = Color::Green;
+            break;
+
+        case 2:
+            label1->Text = "As I see it, yes";
+            label1->ForeColor = Color::Green;
+            break;
+
+        case 3:
+            label1->Text = "Signs point to yes";
+            label1->ForeColor = Color::Green;
+            break;
+
+        case 4:
+            label1->Text = "Very doubtful";
+            label1->ForeColor = Color::Red;
+            break;
+
+        case 5:
+            label1->Text = "Better not";
+            label1->ForeColor = Color::Red;
+            break;
+
+        case 6:
+            label1->Text = "God, no!";
+            label1->ForeColor = Color::Red;
+            break;
+
+        case 7:
+            label1->Text = "Maybe..";
+            label1->ForeColor = Color::Orange;
+            break;
+
+        case 8:
+            label1->Text = "You can try :)";
+            label1->ForeColor = Color::Orange;
+            break;
     }
-    else if (RandNumb > 25 && RandNumb <= 37.5)
-    {
-        label1->Text = "Signs point to yes";
-        label1->ForeColor = Color::Green;
-    }
-    else if (RandNumb > 37.5 && RandNumb <= 50)
-    {
-        label1->Text = "Very doubtful";
-        label1->ForeColor = Color::Red;
-    }
-    else if (RandNumb > 50 && RandNumb <= 62.5)
-    {
-        label1->Text = "Better not";
-        label1->ForeColor = Color::Red;
-    }
-    else if (RandNumb > 62.5 && RandNumb <= 75)
-    {
-        label1->Text = "God, no!";
-        label1->ForeColor = Color::Red;
-    }
-    else if (RandNumb > 75 && RandNumb <= 87.5)
-    {
-        label1->Text = "Maybe..";
-        label1->ForeColor = Color::Orange;
-    }
-    else
-    {
-        label1->Text = "You can try :)";
-        label1->ForeColor = Color::Orange;
-    }
+
 }
